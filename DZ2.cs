8b1d23bec4b7fc97c90b2b5864d00bf6961ccf36
SotMirror <110252714+SotMirror@users.noreply.github.com>
2022-09-04 15:44:53 +0300

    Add files via upload

---
+++ DZ2.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.Write("Введите число для вычесления суммы цифры в числе: ");
+int sum = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (sum > 0)
+    {
+        result += sum % 10;
+        sum /= 10;
+    }
+Console.WriteLine(result);
---
+++ DZ3.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Ввeдите количество цифр с случайном масиве ");
+int A=Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int [A];
+FillArray(numbers);
+PrintArray(numbers);
+void FillArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next();
+}
+}
+
+void PrintArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+}
